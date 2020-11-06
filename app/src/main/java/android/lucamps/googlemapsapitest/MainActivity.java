04232f4d238124d37a1689a0d49d8b1554264023
lucamps <lucas.campos2@ufv.br>
2020-11-06 19:36:07 -0300

    Adding Google Maps API certificate & testing new activity (MapsActivity).

--- app/src/main/java/android/lucamps/googlemapsapitest/MainActivity.java
+++ app/src/main/java/android/lucamps/googlemapsapitest/MainActivity.java
@@ -2,7 +2,9 @@ package android.lucamps.googlemapsapitest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button myLocation_bt = findViewById(R.id.bt_myLocation);
+
+        myLocation_bt.setOnClickListener(view -> {
+            Intent it = new Intent (getBaseContext(), MapsActivity.class);
+            startActivity(it);
+        });
     }
 }
