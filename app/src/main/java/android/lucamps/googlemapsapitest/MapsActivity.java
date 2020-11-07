76716aaab7e2f8cbabfc6eee989f086925668edf
lucamps <lucas.campos2@ufv.br>
2020-11-07 08:56:48 -0300

    Testing GPS features

--- app/src/main/java/android/lucamps/googlemapsapitest/MapsActivity.java
+++ app/src/main/java/android/lucamps/googlemapsapitest/MapsActivity.java
@@ -2,13 +2,23 @@ package android.lucamps.googlemapsapitest;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -20,30 +30,106 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import org.w3c.dom.Text;
 
+import java.text.DecimalFormat;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-    private LatLng APT = new LatLng(-20.751036, -42.869928);
-    private LatLng PF = new LatLng(-20.672017, -43.081624);
-    private LatLng DPI = new LatLng(-20.764962, -42.868489);
-
-    //private String APT_title = getString(R.string.apartment);
-   /* private String PF_title = getString(R.string.pf_house);
-    private String DPI_title = getString(R.string.department);*/
-
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
+    private final LatLng APT = new LatLng(-20.751036, -42.869928);
+    private final LatLng PF = new LatLng(-20.672017, -43.081624);
+    private final LatLng DPI = new LatLng(-20.764962, -42.868489);
     private GoogleMap mMap;
+
+    public LocationManager lm;
+    public Criteria criteria;
+    public String provider;
+    public int REQUISITION_TIME_LATLONG = 5000;
+    public int DISTANCE_IN_METERS = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        // Location Manager
+        lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        criteria = new Criteria();
+
+        // Tests if has GPS
+        PackageManager packageManager = getPackageManager();
+        boolean hasGPS = packageManager.hasSystemFeature(PackageManager.FEATURE_LOCATION_GPS);
+
+        // Sets criteria accuracy
+        if (hasGPS) {
+            criteria.setAccuracy(Criteria.ACCURACY_FINE);
+            Log.i("LOCATION", "Using GPS");
+        } else {
+            criteria.setAccuracy(Criteria.ACCURACY_COARSE);
+            Log.i("LOCATION", "Using internet");
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // Gets the best provider
+        provider = lm.getBestProvider(criteria, true);
+
+        if (provider == null) {
+            Log.e("PROVIDER", "Provider not found!");
+        } else {
+            Log.i("PROVIDER", "Using: " + provider);
+
+            // Gets location update
+            lm.requestLocationUpdates(provider, REQUISITION_TIME_LATLONG, DISTANCE_IN_METERS, this);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        // Stops location manager
+        lm.removeUpdates(this);
+
+        Log.w("PROVIDER","Provider " + provider + " stopped!");
+        super.onDestroy();
+    }
+
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {}
+
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {}
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        // Creates a random point
+        final Location randomPoint = new Location(provider);
+        randomPoint.setLatitude(DPI.latitude);
+        randomPoint.setLongitude(DPI.longitude);
+
+        double lat = location.getLatitude();
+        double lng = location.getLongitude();
+        double speed = location.getSpeed() * 3.6; //converts into km/h
+        double distance = location.distanceTo(randomPoint) / 1000; //converts into km
+
+        DecimalFormat df = new DecimalFormat("0.##");
+
+        Toast.makeText(this, "Latitude: " + lat +
+                                            "\nLongitude: " + lng +
+                                            "\nVelocidade: " + df.format(speed) +
+                                            "Distancia: " + df.format(distance),
+                                            Toast.LENGTH_LONG).show();
     }
 
     /**
@@ -63,6 +149,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.addMarker(new MarkerOptions().position(APT).title(getString(R.string.apartment)));
         mMap.addMarker(new MarkerOptions().position(PF).title(getString(R.string.pf_house)));
         mMap.addMarker(new MarkerOptions().position(DPI).title(getString(R.string.department)));
+        //mMap.addMarker(new MarkerOptions().position(lm.))
 
         CameraUpdate update;
         switch (it.getStringExtra("local")){
